f4a29eeb9c2f5681a74e9ce5c624c267fa7adf26
MinCheol-Yoon <democracy56@naver.com>
2022-10-07 20:00:15 +0900

    delete

--- BackEnd/rest_api/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class OnlineJudge(models.Model):
-    user = models.CharField(max_length=70, blank=False, default='')
-    code = models.CharField(max_length=20000,blank=False, default='')
-    isPassed = models.BooleanField(default=False)
