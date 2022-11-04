47344add4858ce004748e99283ff94def7448ecc
PeaceCodemath <2020project@naver.com>
2022-11-05 00:03:53 +0900

    add api_key

--- back/modules/code_explain.py
+++ back/modules/code_explain.py
@@ -1,17 +1,14 @@
 import os
 import openai
+from dotenv import load_dotenv
 
 class Explain:
     def act(text):
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        #이거 key를 openai에서 받아와야 하는데 방법을 모르겠음.
-        #API 키 받아서 입력
-
+        load_dotenv()
+        openai.api_key = os.getenv("API_KEY")
         response = openai.Completion.create(
             model="code-davinci-002",
-            prompt=text+ "\n\"\"\"\nThe code is doing the following:\n",
-            #앞의 file에 파이썬 코드 입력 필요
-            #그리고 뒤의 부분과 합쳐야 코드가 돌아감
+            prompt=text+ "\n\"\"\"\nThe code is doing the following:\n1:",
             temperature=0,
             max_tokens=64,
             top_p=1.0,
@@ -19,7 +16,8 @@ class Explain:
             presence_penalty=0.0,
             stop=["\"\"\""]
         )
-        return response
+        result = "" + response
+        return result
         
 
 #From example of openai(openai.com)
