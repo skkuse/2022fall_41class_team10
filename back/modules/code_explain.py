b82a11a1b5f3b566a4732a7d31d1085a4f9c7e3e
PeaceCodemath <2020project@naver.com>
2022-11-04 15:56:35 +0900

    change code_explain

--- back/modules/code_explain.py
+++ back/modules/code_explain.py
@@ -2,17 +2,14 @@ import os
 import openai
 
 class Explain:
-    def __init__(self):
-        self.response = ""
-
-    def act(self, text):
+    def act(text):
         openai.api_key = os.getenv("OPENAI_API_KEY")
         #이거 key를 openai에서 받아와야 하는데 방법을 모르겠음.
         #API 키 받아서 입력
 
-        self.response = openai.Completion.create(
+        response = openai.Completion.create(
             model="code-davinci-002",
-            prompts=text+ "\n \"\"\"\n The code is doing the following:\n",
+            prompt=text+ "\n \"\"\"\n The code is doing the following:\n",
             #앞의 file에 파이썬 코드 입력 필요
             #그리고 뒤의 부분과 합쳐야 코드가 돌아감
             temperature=0,
@@ -22,7 +19,7 @@ class Explain:
             presence_penalty=0.0,
             stop=["\"\"\""]
         )
-        return self.response
+        return response
         
 
 #From example of openai(openai.com)
