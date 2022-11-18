99bb77a3b8e6b79079ccddfa6ec64a9c546dadac
PeaceCodemath <2020project@naver.com>
2022-11-18 21:52:50 +0900

    fix code_explain

--- back/modules/code_explain.py
+++ back/modules/code_explain.py
@@ -8,7 +8,7 @@ class Explain:
         openai.api_key = os.getenv("API_KEY")
         response = openai.Completion.create(
             model="code-davinci-002",
-            prompt=text+ "\n\"\"\"\nThe code is doing the following:\n1:",
+            prompt=text+ "\n\"\"\"\nHere's what the above class is doing:\n",
             temperature=0,
             max_tokens=64,
             top_p=1.0,
@@ -16,8 +16,7 @@ class Explain:
             presence_penalty=0.0,
             stop=["\"\"\""]
         )
-        result = "" + response
-        return result
+        return response["choices"][0]["text"]
         
 
 #From example of openai(openai.com)
