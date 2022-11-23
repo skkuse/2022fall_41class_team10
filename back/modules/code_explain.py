4fb47a48706b41f3b9db0ce9d1f7437c6ac468bd
PeaceCodemath <2020project@naver.com>
2022-11-23 16:03:32 +0900

    edit code_explain

--- back/modules/code_explain.py
+++ back/modules/code_explain.py
@@ -8,7 +8,7 @@ class Explain:
         openai.api_key = os.getenv("API_KEY")
         response = openai.Completion.create(
             model="code-davinci-002",
-            prompt=text+ "\n\"\"\"\nHere's what the above class is doing:\n",
+            prompt="class Log:\n" + text + "\n\"\"\"\nHere's what the above class is doing:\n",
             temperature=0,
             max_tokens=64,
             top_p=1.0,
