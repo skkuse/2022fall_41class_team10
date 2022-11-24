0148a5ea79496134113f780cc9ad19e4c8896168
PeaceCodemath <2020project@naver.com>
2022-11-24 19:22:29 +0900

    add parsing.py

---
+++ back/modules/parsing.py
@@ -0,0 +1,13 @@
+def parsing():
+    f = open("./save.txt", 'r')
+    lines = f.readlines()
+    count = 1
+    error_sentence = ""
+    for line in lines:
+        if count >= 5 and line.startswith('-----'):
+            break
+        if count >= 5:
+            error_sentence += line
+        count += 1
+    return error_sentence
+#print(parsing())
