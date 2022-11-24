0aaaddcde348b5965dd79a8cbc96763907b2194b
PeaceCodemath <2020project@naver.com>
2022-11-24 19:09:00 +0900

    change code_execute.py

--- back/modules/code_execute.py
+++ back/modules/code_execute.py
@@ -24,6 +24,7 @@ code1 = ""
 class CodeExecute(unittest.TestCase):
 
     def code_execute(code):
+        #code = "a = 2\nprint(b)" #일단 임의로 지정함
         global code1
         code1 = code
         old_stdout = sys.stdout
@@ -37,15 +38,38 @@ class CodeExecute(unittest.TestCase):
         try:
             exec(code)
             sys.stdout = old_stdout
+            sys.stderr = old_stderr
             result = redirected_output.getvalue()
         except:
+            sys.stdout = old_stdout
+            sys.stderr = old_stderr
             result = "Failed to execute command"
         
         return result
 
     def test(self):
         result = CodeExecute.code_execute(code1)
-        self.assertEqual(result.strip(), answer)
+        if result == "Failed to execute command":
+            with self.assertRaises(IOError):
+                exec(code1)
+        else:
+            self.assertEqual(result.strip(), answer)
+
+'''
+def makeSuite(testcase, tests):
+    return unittest.TestSuite(map(testcase, tests))
+'''
 
 if __name__ == "__main__":
-    unittest.main()
+    '''
+    suite = makeSuite(CodeExecute,['test'])
+
+    allsuites = unittest.TestSuite([suite])
+    redirected_result = StringIO()
+    unittest.TextTestRunner(verbosity = 2).run(allsuites)
+    result = redirected_result.getvalue()
+    '''
+    path = "./save.txt"
+    old_stderr = sys.stderr
+    with open(path,'w') as sys.stderr:
+        unittest.main()
