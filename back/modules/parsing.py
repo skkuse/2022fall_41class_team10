7295c46784bf7d1d6f9bfda3f9c24650d994bf13
PeaceCodemath <2020project@naver.com>
2022-11-25 14:13:55 +0900

    edit parsing.py

--- back/modules/parsing.py
+++ back/modules/parsing.py
@@ -1,14 +1,14 @@
 def parsing():
     f = open("./save.txt", 'r')
     lines = f.readlines()
-    count = 1
     error_sentence = ""
-    for line in lines:
-        if count == 1 and line.startswith('E'):
+    if lines[0].startswith('E'):
+        count = 1
+        for line in lines:
             if count >= 5 and line.startswith('-----'):
                 break
             if count >= 5:
                 error_sentence += line
-        count += 1
+            count = count + 1
     return error_sentence
 #print(parsing())
