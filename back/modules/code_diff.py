6a8b50972cb8985c4ee75017b27fdc762554121c
skfsjrnfl <81664471+skfsjrnfl@users.noreply.github.com>
2022-12-11 19:15:58 +0900

    [Back][Fix] Fix typo

--- back/modules/code_diff.py
+++ back/modules/code_diff.py
@@ -3,7 +3,7 @@ import os
 class CodeDiff:
     def MakeDiffStr(class_id, assign_id, file_path):
         dir_path = "./data/class_%d/assign_%d" % (class_id, assign_id)
-        aws_path = dir_path +"/anwser.py"
+        aws_path = dir_path +"/answer.py"
         command='git diff --no-index --output=diffstr.txt {0} {1}'.format(file_path, aws_path)
         os.system(command)
         output_file=open("diffstr.txt","r")
