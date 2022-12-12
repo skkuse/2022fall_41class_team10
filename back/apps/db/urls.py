fdbdd9a6f0a0d6c6141e221f920bab5456a7a77e
skfsjrnfl <81664471+skfsjrnfl@users.noreply.github.com>
2022-12-12 21:16:19 +0900

    [Back][Add] Add db api for eng data

--- back/apps/db/urls.py
+++ back/apps/db/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.get_class_list, name='get_class_list'),
     path('<int:class_id>', views.get_assignment_list, name='get_assignment_list'),
+    path('<int:class_id>/eng', views.get_assignment_list_eng, name='get_assignment_list_eng'),
     path('<int:class_id>/<int:assign_id>', views.get_assignment_info, name="get_assignment_info"),
+    path('<int:class_id>/<int:assign_id>/eng', views.get_assignment_info_eng, name="get_assignment_info_eng"),
     path('<int:class_id>/<int:assign_id>/testcase', views.get_testcase_list, name="get_testcase_list"),
     path('<int:class_id>/<int:assign_id>/explain', views.get_explain, name="get_explain"),
     path('save',views.save_user_code, name="save_user_code"),
